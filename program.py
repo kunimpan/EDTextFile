d1f157889d377036a314110aed58fe8fe4322022
Pun <impan_1611_2544@hotmail.com>
2024-02-03 14:40:38 +0700

    Edit:use scrolltext

--- program.py
+++ program.py
@@ -2,25 +2,29 @@ from tkinter import *
 from tkinter.filedialog import *
 from cryptography.fernet import Fernet
 import tkinter.messagebox
+from tkinter import scrolledtext
 
 root=Tk()
 root.title("Program Encrypt-Decrypt")
-root.geometry("500x350")
+root.geometry("500x500")
 root.resizable(0, 0)
 
 # Generate a symmetric key
 key = Fernet.generate_key()
-plain = b'' 
-cipherData = b''
+
+plain = b'' # Original text
+cipherData = b'' # Encrypted original text
 
 def openText():
     myFile = askopenfilename(initialdir="./", title="Open note", filetypes=(("Text File", "*.txt"), ("All File", "*")))
     with open(myFile, "rb") as file:
-        #global plain
+        global plain
         plain = file.read()
-        plain_txt.insert(0, plain.decode('ascii'))
+        plain_txt.insert("1.0", plain.decode('ascii'))
 
 def encrypt():
+    temp = plain_txt.get("1.0", tkinter.END)
+    plain = bytes(temp, 'utf-8')
 
     # Create "secretKey.key" for keep key.
     with open('secretKey.key', 'wb') as file:
@@ -35,7 +39,7 @@ def encrypt():
     f = Fernet(genKey)
     global encryptedData
     encryptedData = f.encrypt(plain) # encrypted
-    encrypted_txt.insert(0, encryptedData)
+    encrypted_txt.insert("1.0", encryptedData)
 
     with open('cipherText.txt', 'wb') as file:
         file.write(encryptedData)
@@ -46,22 +50,25 @@ def encrypt():
 def openCipher():
     cipherFile = askopenfilename(initialdir="./", title="Open Cipher", filetypes=(("Text File", "*.txt"), ("All File", "*")))
     with open(cipherFile, 'rb') as file:
-        #global cipherData
+        global cipherData
         cipherData = file.read()
         cipher_txt.insert(0, cipherData)
 
 def decrypt():
+    cipherData = cipher_txt.get("1.0", tkinter.END)
+    print(cipherData)
     keyDecrypt = Fernet(decrypt_key_txt.get()) # Decrypt key
     decryptedData = keyDecrypt.decrypt(cipherData) # Decrypt
-    decrypt_cipher_txt.insert(0,  decryptedData.decode())
+    print(decryptedData.decode())
+    decrypt_cipher_txt.insert("1.0", decryptedData.decode())
 
 #design frame
 btnFrame=LabelFrame(root, text="Menu")
 EncryptFrame=LabelFrame(root, text="Encrypt")
 DecryptFrame=LabelFrame(root, text="Decrypt")
-btnFrame.grid(row=0, column=0, columnspan=2, ipadx=200, pady=5)
-EncryptFrame.grid(row=1, column=0)
-DecryptFrame.grid(row=2, column=0)
+btnFrame.pack()
+EncryptFrame.pack(pady=10)
+DecryptFrame.pack(pady=10)
 
 #button widget
 btnOpen=Button(btnFrame, text="Open file", command=openText)
@@ -78,32 +85,36 @@ btnDecrypt.grid(row=0, column=3, padx=5, pady=5)
 
 #encrypt input widget
 plain_lable = Label(EncryptFrame, text="Plain text")
-plain_txt = Entry(EncryptFrame, width=50)
-encrypted_lable = Label(EncryptFrame, text="Encrypted text")
-encrypted_txt = Entry(EncryptFrame, width=50)
-key_lable= Label(EncryptFrame, text="Key")
-key_txt = Entry(EncryptFrame, width=50)
+plain_txt = scrolledtext.ScrolledText(EncryptFrame, bg="white", width = 37, height = 1)
+encrypted_lable = Label(EncryptFrame, text="Cipher text")
+encrypted_txt = scrolledtext.ScrolledText(EncryptFrame, bg="white", width = 37, height = 1)
+key_lable= Label(EncryptFrame, text="Decrypt key")
+key_txt = Entry(EncryptFrame, width=52)
 
-plain_lable.grid(row=0, column=0, padx=5, pady=5)
-plain_txt.grid(row=0, column=1)
-encrypted_lable.grid(row=1, column=0, padx=5, pady=5)
-encrypted_txt.grid(row=1, column=1)
-key_lable.grid(row=2, column=0, padx=5, pady=5)
+
+plain_lable.grid(row=0, column=0, padx=5, pady=5, sticky=NW)
+plain_txt.grid(row=0, column=1, padx=2, pady=2)
+encrypted_lable.grid(row=1, column=0, padx=5, pady=5, sticky=NW)
+encrypted_txt.grid(row=1, column=1, padx=2, pady=2)
+key_lable.grid(row=2, column=0, padx=5, pady=5, sticky=W)
 key_txt.grid(row=2, column=1)
+
+#textArea_lable.grid(row=3, column=0, padx=5, pady=5, sticky=N)
+#textArea.grid(row=3, column=1)
 
 #decrypt input widget
 cipher_lable = Label(DecryptFrame, text="Cipher text")
-cipher_txt = Entry(DecryptFrame, width=50)
+cipher_txt = scrolledtext.ScrolledText(DecryptFrame, bg="white", width = 37, height = 1)
 decrypt_key_lable = Label(DecryptFrame, text="Decrypt key")
-decrypt_key_txt = Entry(DecryptFrame, width=50)
-decrypt_cipher_lable = Label(DecryptFrame, text="PlainD text")
-decrypt_cipher_txt = Entry(DecryptFrame, width=50)
+decrypt_key_txt = Entry(DecryptFrame, width=52)
+decrypt_cipher_lable = Label(DecryptFrame, text="Plain text")
+decrypt_cipher_txt = scrolledtext.ScrolledText(DecryptFrame, bg="white", width = 37, height = 1)
 
-cipher_lable.grid(row=0, column=0, padx=5, pady=5)
-cipher_txt.grid(row=0, column=1)
-decrypt_key_lable.grid(row=1, column=0, padx=5, pady=5)
+cipher_lable.grid(row=0, column=0, padx=5, pady=5, sticky=NW)
+cipher_txt.grid(row=0, column=1, padx=2, pady=2)
+decrypt_key_lable.grid(row=1, column=0, padx=5, pady=5, sticky=W)
 decrypt_key_txt.grid(row=1, column=1)
-decrypt_cipher_lable.grid(row=2, column=0, padx=5, pady=5)
-decrypt_cipher_txt.grid(row=2, column=1)
+decrypt_cipher_lable.grid(row=2, column=0, padx=5, pady=5, sticky=NW)
+decrypt_cipher_txt.grid(row=2, column=1, padx=2, pady=2)
 
 root.mainloop()
